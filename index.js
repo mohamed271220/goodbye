b3e662b17abf2e830e634ea8eaf2e131e992747b
mohamed271220 <mohamedaprahym@gmail.com>
2023-02-18 16:34:13 +0200

    first

---
+++ index.js
@@ -0,0 +1,18 @@
+document.querySelector("h1").innerHTML="goodbye"
+document.querySelector("h1").classList.add("huge")
+document.querySelector("input").click()
+document.getElementsByClassName("item")[2].innerHTML="mohamed"
+document.getElementById("list").style.backgroundColor='red'
+document.querySelector("li")
+document.querySelector("#item")
+document.querySelector(".item")
+document.querySelector("li a")
+document.querySelector("#list a").style.color="blue"
+document.querySelectorAll("#list a")
+document.querySelector("li.item")
+document.querySelector(".btn").style.backgroundColor='yellow'
+document.querySelector("button").classList.add("whatever")
+document.querySelector("button").classList.toggle("whatever")
+//
+document.querySelector("a").attributes
+document.querySelector("a").setAttribute("href","www.google.com")
